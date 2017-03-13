84353b2c34f16a47698b64ca9f114b4cc72ab4a7
Andrew Reidmeyer <areidmeyer@nvidia.com>
2017-03-13 12:52:44 -0600

    NvFlow. Library update. Documentation.

--- Engine/Plugins/GameWorks/NvFlow/Include/include/NvFlowContextD3D12.h
+++ Engine/Plugins/GameWorks/NvFlow/Include/include/NvFlowContextD3D12.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008-2016, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2014-2017, NVIDIA CORPORATION.  All rights reserved.
  *
  * NVIDIA CORPORATION and its licensors retain all intellectual property
  * and proprietary rights in and to this software, related documentation
@@ -9,6 +9,10 @@
  */
 
 #pragma once
+
+// --------------------------- NvFlowContextD3D12 -------------------------------
+///@defgroup NvFlowContextD3D12
+///@{
 
 struct NvFlowDepthStencilViewDescD3D12
 {
@@ -72,86 +76,86 @@ struct NvFlowResourceRWViewDescD3D12
 	D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc;
 };
 
-// --------------------------- NvFlowContext -------------------------------
-
 /**
-* Creates a graphics/compute context for Flow.
-*
-* @param[in] version Should be set by app to NV_FLOW_VERSION.
-* @param[in] desc A graphics-API dependent structure containing data needed for a FlowContext to interoperate with the app.
-*
-* @return The created Flow context.
-*/
+ * Creates a graphics/compute context for Flow.
+ *
+ * @param[in] version Should be set by app to NV_FLOW_VERSION.
+ * @param[in] desc A graphics-API dependent structure containing data needed for a FlowContext to interoperate with the app.
+ *
+ * @return The created Flow context.
+ */
 NV_FLOW_API NvFlowContext* NvFlowCreateContextD3D12(NvFlowUint version, const NvFlowContextDescD3D12* desc);
 
 /**
-* Creates a Flow depth stencil view based on information provided by the application.
-*
-* @param[in] context The Flow context to create and use the depth stencil view.
-* @param[in] desc The graphics API dependent description.
-*
-* @return The created Flow depth stencil view.
-*/
+ * Creates a Flow depth stencil view based on information provided by the application.
+ *
+ * @param[in] context The Flow context to create and use the depth stencil view.
+ * @param[in] desc The graphics API dependent description.
+ *
+ * @return The created Flow depth stencil view.
+ */
 NV_FLOW_API NvFlowDepthStencilView* NvFlowCreateDepthStencilViewD3D12(NvFlowContext* context, const NvFlowDepthStencilViewDescD3D12* desc);
 
 /**
-* Creates a Flow render target view based on information provided by the application.
-*
-* @param[in] context The Flow context to create and use the render target view.
-* @param[in] desc The graphics API dependent description.
-*
-* @return The created Flow render target view.
-*/
+ * Creates a Flow render target view based on information provided by the application.
+ *
+ * @param[in] context The Flow context to create and use the render target view.
+ * @param[in] desc The graphics API dependent description.
+ *
+ * @return The created Flow render target view.
+ */
 NV_FLOW_API NvFlowRenderTargetView* NvFlowCreateRenderTargetViewD3D12(NvFlowContext* context, const NvFlowRenderTargetViewDescD3D12* desc);
 
 /**
-* Updates a Flow context with information provided by the application.
-*
-* @param[in] context The Flow context to update.
-* @param[in] desc The graphics API dependent description.
-*/
+ * Updates a Flow context with information provided by the application.
+ *
+ * @param[in] context The Flow context to update.
+ * @param[in] desc The graphics API dependent description.
+ */
 NV_FLOW_API void NvFlowUpdateContextD3D12(NvFlowContext* context, const NvFlowContextDescD3D12* desc);
 
 /**
-* Gets a Flow context description from a Flow context.
-*
-* @param[in] context The Flow context.
-* @param[out] desc The graphics API dependent description.
-*/
+ * Gets a Flow context description from a Flow context.
+ *
+ * @param[in] context The Flow context.
+ * @param[out] desc The graphics API dependent description.
+ */
 NV_FLOW_API void NvFlowUpdateContextDescD3D12(NvFlowContext* context, NvFlowContextDescD3D12* desc);
 
 /**
-* Updates a Flow depth stencil view with information provided by the application.
-*
-* @param[in] context The Flow context used to create the depth stencil view.
-* @param[in] view The Flow depth stencil view to update.
-* @param[in] desc The graphics API dependent description.
-*/
+ * Updates a Flow depth stencil view with information provided by the application.
+ *
+ * @param[in] context The Flow context used to create the depth stencil view.
+ * @param[in] view The Flow depth stencil view to update.
+ * @param[in] desc The graphics API dependent description.
+ */
 NV_FLOW_API void NvFlowUpdateDepthStencilViewD3D12(NvFlowContext* context, NvFlowDepthStencilView* view, const NvFlowDepthStencilViewDescD3D12* desc);
 
 /**
-* Updates a Flow render target view with information provided by the application.
-*
-* @param[in] context The Flow context used to create the render target view.
-* @param[in] view The Flow render target view to update.
-* @param[in] desc The graphics API dependent description.
-*/
+ * Updates a Flow render target view with information provided by the application.
+ *
+ * @param[in] context The Flow context used to create the render target view.
+ * @param[in] view The Flow render target view to update.
+ * @param[in] desc The graphics API dependent description.
+ */
 NV_FLOW_API void NvFlowUpdateRenderTargetViewD3D12(NvFlowContext* context, NvFlowRenderTargetView* view, const NvFlowRenderTargetViewDescD3D12* desc);
 
 /**
-* Updates an application visible description with internal Flow resource information.
-*
-* @param[in] context The Flow context that created the resource.
-* @param[in] resource The Flow resource to describe.
-* @param[out] desc The graphics API dependent Flow resource description.
-*/
+ * Updates an application visible description with internal Flow resource information.
+ *
+ * @param[in] context The Flow context that created the resource.
+ * @param[in] resource The Flow resource to describe.
+ * @param[out] desc The graphics API dependent Flow resource description.
+ */
 NV_FLOW_API void NvFlowUpdateResourceViewDescD3D12(NvFlowContext* context, NvFlowResource* resource, NvFlowResourceViewDescD3D12* desc);
 
 /**
-* Updates an application visible description with internal Flow resourceRW information.
-*
-* @param[in] context The Flow context that created the resourceRW.
-* @param[in] buffer The Flow resourceRW to describe.
-* @param[out] desc The graphics API dependent Flow resourceRW description.
-*/
+ * Updates an application visible description with internal Flow resourceRW information.
+ *
+ * @param[in] context The Flow context that created the resourceRW.
+ * @param[in] buffer The Flow resourceRW to describe.
+ * @param[out] desc The graphics API dependent Flow resourceRW description.
+ */
 NV_FLOW_API void NvFlowUpdateResourceRWViewDescD3D12(NvFlowContext* context, NvFlowResourceRW* resourceRW, NvFlowResourceRWViewDescD3D12* desc);
+
+///@}
--- Engine/Plugins/GameWorks/NvFlow/Include/include/NvFlowShader.h
+++ Engine/Plugins/GameWorks/NvFlow/Include/include/NvFlowShader.h
@@ -1,12 +1,12 @@
 /*
-* Copyright (c) 2008-2016, NVIDIA CORPORATION.  All rights reserved.
-*
-* NVIDIA CORPORATION and its licensors retain all intellectual property
-* and proprietary rights in and to this software, related documentation
-* and any modifications thereto.  Any use, reproduction, disclosure or
-* distribution of this software and related documentation without an express
-* license agreement from NVIDIA CORPORATION is strictly prohibited.
-*/
+ * Copyright (c) 2014-2017, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * NVIDIA CORPORATION and its licensors retain all intellectual property
+ * and proprietary rights in and to this software, related documentation
+ * and any modifications thereto.  Any use, reproduction, disclosure or
+ * distribution of this software and related documentation without an express
+ * license agreement from NVIDIA CORPORATION is strictly prohibited.
+ */
 
 #ifndef NV_FLOW_SHADER_H
 #define NV_FLOW_SHADER_H
